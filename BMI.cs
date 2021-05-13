d204247114e331b49540888220406a4be1c6b39c
Admin <Admin@LAPTOP-798FHRV5>
2021-05-13 15:33:16 +1200

    Add project files.

---
+++ BMI.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamBMI
+{
+    [Activity(Label = "BMI")]
+    public class BMI : Activity
+    {
+        EditText txtHeight;
+        EditText txtWeight;
+        EditText txtResult;
+        TextView txtMessage;
+        Button btnCalculate;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.BMI);
+            InitializeControls();
+            // Create your application here
+        }
+        public void InitializeControls()
+        {
+            //Connect the fake controls to the real controls
+            txtHeight = FindViewById<EditText>(Resource.Id.txtHeight);
+            txtWeight = FindViewById<EditText>(Resource.Id.txtWeight);
+            txtResult = FindViewById<EditText>(Resource.Id.txtResult);
+            txtMessage = FindViewById<TextView>(Resource.Id.txtMessage);
+            btnCalculate = FindViewById<Button>(Resource.Id.btnCalculate);
+            //Create a Button Click Method
+            btnCalculate.Click += onBtnCalculateClick;
+        }
+        public void onBtnCalculateClick(object sender, EventArgs e)
+        {
+            if (txtWeight.Text == "")
+            {
+                Toast.MakeText(this, "Please enter the weight", ToastLength.Long).Show();
+                return;
+            }
+            if (txtHeight.Text == "")
+            {
+                Toast.MakeText(this, "Please enter the height", ToastLength.Long).Show();
+                return;
+            }
+            CalcBMI();
+        }
+        private void CalcBMI()
+        {
+            //Stuff happens here
+            double Height = Convert.ToDouble(txtHeight.Text);
+            double Weight = Convert.ToDouble(txtWeight.Text);
+            double BMIAnswer = Weight / (Height * Height);
+            txtResult.Text = Convert.ToString(Math.Round(BMIAnswer, 2));
+            if (BMIAnswer <= 18.5)
+            {
+                txtMessage.Text = "Underweight";
+            }
+            else if (BMIAnswer >= 18.60 && BMIAnswer <= 24.99)
+            {
+                txtMessage.Text = "Normal";
+            }
+            else if (BMIAnswer > 25 && BMIAnswer <= 29.99)
+            {
+                txtMessage.Text = "Overweight";
+            }
+            else if (BMIAnswer > 30)
+            {
+                txtMessage.Text = "Obese ";
+            }
+            //Create a log system, use other tags if you want so you can filter them
+            string tag = "BMI";
+            Log.Info(tag, "Height = " + Height.ToString());
+            Log.Info(tag, "Weight = " + Weight.ToString());
+            Log.Info(tag, "Answer = " + BMIAnswer.ToString());
+            Log.Info(tag, "Message = " + txtMessage.Text);
+        }
+    }
+}
+       
---
+++ MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace XamBMI
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button btnLoadBMI; 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnLoadBMI = FindViewById<Button>(Resource.Id.btnLoadBMI);
+            btnLoadBMI.Click += onBtnLoadBMIClick;
+        }
+
+        private void onBtnLoadBMIClick(object sender, EventArgs e)
+        {
+            StartActivity(typeof(BMI));
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
